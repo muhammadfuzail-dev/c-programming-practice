ff00b86841e07feaa24a1d24e0a3259978d2c84a
Muhammad Fuzail <fuzailmaseed3@gmail.com>
2026-08-17 00:50:13 +0500

    file with globals variables

---
+++ Practice/globals.cpp
@@ -0,0 +1,5 @@
+// ===== globals.cpp =====
+// Ye file ek global variable "sharedCounter" DEFINE karti hai.
+// main.cpp isko "extern" ke through access karega, bina isko dobara declare kiye.
+
+int sharedCounter = 100;   // actual storage yahan bani hai
